4b3b5d36591697c5ad008de7525419da9c5cc9d6
ksm <ksmsergei29@gmail.com>
2025-01-31 09:37:15 +0500

    Fixed nav bar

--- js/load-nav.js
+++ js/load-nav.js
@@ -1,3 +1,4 @@
-$.get("templates/nav.html", function(data) {
+let basePath = window.location.pathname.includes("/iitu-sfb/") ? "/iitu-sfb/" : "/";
+$.get(basePath + "templates/nav.html", function(data) {
     $("body").prepend(data);
 });
