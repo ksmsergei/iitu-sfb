e1456f40b18cc3d4d7c4cd967643936465bacce2
ksm <ksmsergei29@gmail.com>
2025-02-07 00:07:57 +0500

    More improved UI

--- js/load-page-parts.js
+++ js/load-page-parts.js
@@ -2,3 +2,7 @@ let basePath = window.location.pathname.includes("/iitu-sfb/") ? "/iitu-sfb/" : 
 $.get(basePath + "templates/nav.html", function(data) {
     $("body").prepend(data);
 });
+
+$.get(basePath + "templates/footer.html", function(data) {
+    $("body").append(data);
+});
